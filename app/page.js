b48a29ecd3a82d6f653f494b049ba6ff1040c342
ashiii1 <aashishaik526@gmail.com>
2024-06-26 19:31:20 +0530

    shadcn done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>AAshi</h2>
+      <Button>Subscribe</Button>
     </div>
   );
 }
